107f50ef58a1cc414464c2e2fc34deed6c1cf44f
Alexander López <lopez.alexander@pucp.edu.pe>
2022-05-28 15:41:26 -0500

    Add files via upload

--- flatland_plugins/src/diff_drive.cpp
+++ flatland_plugins/src/diff_drive.cpp
@@ -159,16 +159,13 @@ void DiffDrive::OnInitialize(const YAML::Node& config) {
                   odom_pose_noise[1], odom_pose_noise[2], odom_twist_noise[0],
                   odom_twist_noise[1], odom_twist_noise[2], pub_rate);
 }
-
-void DiffDrive::BeforePhysicsStep(const Timekeeper& timekeeper) {
+void DiffDrive::AfterPhysicsStep(const Timekeeper& timekeeper) {
   bool publish = update_timer_.CheckUpdate(timekeeper);
-
-  b2Body* b2body = body_->physics_body_;
-
-  b2Vec2 position = b2body->GetPosition();
-  float angle = b2body->GetAngle();
-
   if (publish) {
+    b2Body* b2body = body_->physics_body_;
+    b2Vec2 position = b2body->GetPosition();
+    float angle = b2body->GetAngle();
+
     // get the state of the body and publish the data
     b2Vec2 linear_vel_local =
         b2body->GetLinearVelocityFromLocalPoint(b2Vec2(0, 0));
@@ -180,8 +177,8 @@ void DiffDrive::BeforePhysicsStep(const Timekeeper& timekeeper) {
     ground_truth_msg_.pose.pose.position.z = 0;
     ground_truth_msg_.pose.pose.orientation =
         tf::createQuaternionMsgFromYaw(angle);
-    ground_truth_msg_.twist.twist.linear.x = linear_vel_local.x;
-    ground_truth_msg_.twist.twist.linear.y = linear_vel_local.y;
+    ground_truth_msg_.twist.twist.linear.x = sqrt(pow(linear_vel_local.x,2) + pow(linear_vel_local.y,2));
+    ground_truth_msg_.twist.twist.linear.y = 0.0; //linear_vel_local.y;
     ground_truth_msg_.twist.twist.linear.z = 0;
     ground_truth_msg_.twist.twist.angular.x = 0;
     ground_truth_msg_.twist.twist.angular.y = 0;
@@ -196,7 +193,7 @@ void DiffDrive::BeforePhysicsStep(const Timekeeper& timekeeper) {
     odom_msg_.pose.pose.orientation =
         tf::createQuaternionMsgFromYaw(angle + noise_gen_[2](rng_));
     odom_msg_.twist.twist.linear.x += noise_gen_[3](rng_);
-    odom_msg_.twist.twist.linear.y += noise_gen_[4](rng_);
+    odom_msg_.twist.twist.linear.y == 0.0; //noise_gen_[4](rng_);
     odom_msg_.twist.twist.angular.z += noise_gen_[5](rng_);
 
     if (enable_odom_pub_) {
@@ -231,6 +228,14 @@ void DiffDrive::BeforePhysicsStep(const Timekeeper& timekeeper) {
     odom_tf.transform.rotation = odom_msg_.pose.pose.orientation;
     tf_broadcaster.sendTransform(odom_tf);
   }
+
+
+}
+void DiffDrive::BeforePhysicsStep(const Timekeeper& timekeeper) {
+  b2Body* b2body = body_->physics_body_;
+
+  b2Vec2 position = b2body->GetPosition();
+  float angle = b2body->GetAngle();
 
   // we apply the twist velocities, this must be done every physics step to make
   // sure Box2D solver applies the correct velocity through out. The velocity
